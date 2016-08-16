beeca9c60e28378d4fb8007cd3382ab813185399
sofiamatulis <sosomatulis@hotmail.com>
2016-08-16 13:54:18 -0400

    test the calculator for add subtract and sum

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,17 @@
+def add(num1,num2)
+  num1 + num2
+end
+
+
+def subtract(num1,num2)
+  num1-num2
+end
+
+
+def sum(total)
+  result = 0
+   total.each do |number|
+  result += number
+  end
+  return result
+end
