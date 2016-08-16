97b1432a9c4b46aec29eb534ba1519bae371fbf8
sofiamatulis <sosomatulis@hotmail.com>
2016-08-16 17:17:01 -0400

    created class friend and greet

--- 05_hello_friend/friend.rb
+++ 05_hello_friend/friend.rb
@@ -1,28 +1,7 @@
-# def self.setup
-#   expected_value = 10
-#   actual_value =Temperature.f_to_c(50)
-#
-#   assert_equal(expected_value,actual_value)
-# end
+
 class Friend
-def initialize(name)
-  @name= name
-  @friend = []
-
+def greet(friend="")
+  "Hello #{friend}!"
 end
 
-
-def self.setup(name)
-
-  new_friend = Contact.new(name)
-  @friend << new_friend
-  return new_contact
-
-
 end
-
-def self.greet(name=?)
-  "Hello #{name} !"
-end
-
-end 
