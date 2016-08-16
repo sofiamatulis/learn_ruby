5a36963c732b7af24504c22f66971d4f0ef66a96
sofiamatulis <sosomatulis@hotmail.com>
2016-08-16 14:32:20 -0400

    created factorial

--- 02_calculator/test_calculator.rb
+++ 02_calculator/test_calculator.rb
@@ -45,24 +45,31 @@ class TestCalculator < MiniTest::Test
   # If you feel you're up for the challenge, fill in the missing code in the tests below,
   # based off the descriptions, and get them to pass.
   def test_multiplies_two_numbers
+    assert_equal 4, multiply([2,2])
   end
 
   def test_multiplies_several_numbers
+    assert_equal 8, multiply([2,2,2])
   end
 
 # http://en.wikipedia.org/wiki/Factorial
   def test_computes_factorial_0
+    assert_equal 0, factorial(0)
   end
 
   def test_computes_factorial_1
+    assert_equal 1, factorial(1)
   end
 
   def test_computes_factorial_2
+    assert_equal 2, factorial(2)
   end
 
   def test_computes_factorial_5
+    assert_equal 120, factorial(5)
   end
 
   def test_computes_factorial_10
+    assert_equal 3628800,factorial(10)
   end
 end
