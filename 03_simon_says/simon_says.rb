8a6eebc6cc495333ccd2aa62d929523f3ae02e94
sofiamatulis <sosomatulis@hotmail.com>
2016-08-16 16:04:34 -0400

    first word works

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -20,9 +20,18 @@ end
 
 
 
-
-
-
 def start_of_word(word, num)
   word[0,num]
+end
+
+def first_word(sentence)
+  sentence.split.first
+end
+
+
+def titleize(words)
+  words.each do |word|
+  word.capitalize
+  if word == "war" || word == "and" || word == 
+end
 end
